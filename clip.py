b65491c6985df123fcff0acab5298b8a2988bedd
Raathore4 <73769538+Raathore4@users.noreply.github.com>
2023-05-15 14:26:17 +0530

    Update clip.py

--- clip.py
+++ clip.py
@@ -1,45 +1,43 @@
 import os
 
+# Path of subBasin Vector File which have all the subBasins
 pathSub = r'H:\AnkitKailashi\dem\carto10m\hydrology\bqasin\subBasins\subBasins.shp'
+
+#define as vector
 v3Man10SubBasin = QgsVectorLayer(pathSub,"subBasin",'ogr')
 
-selectedFeat = []
+feat = []
 
-#v3ManLayer = iface.activeLayer()
-print("Qgs ", v3Man10SubBasin.type())
 
-count =0
-
+#arrange attribute table of sub-basin according the gridcode (a field in their attribute)
 confi = v3Man10SubBasin.attributeTableConfig()
 confi.setSortExpression("gridcode")
 confi.setSortOrder(1)
 v3Man10SubBasin.setAttributeTableConfig(confi)
 
-progressMessageBar = iface.messageBar().createMessage("Doing something boring...")
-progress = QProgressBar()
-progress.setMaximum(100)
-progress.setAlignment(Qt.AlignLeft|Qt.AlignVCenter)
-progressMessageBar.layout().addWidget(progress)
 
-
+# Loop through every sub-basin in the vector file
 for feature in v3Man10SubBasin.getFeatures():
-    iface.messageBar().pushWidget(progressMessageBar, Qgis.Info)
-    iface.statusBarIface().showMessage("Processind")
+    #make a sql expression
     exp = "\"Name\"='{}'".format(feature["Name"])
     v3Man10SubBasin.selectByExpression(exp)
     print(feature["Name"])
-    
-    os.makedirs(r'H:\AnkitKailashi\withCode\Analysis\hypsometric\subBasins\%s'%(feature['Name']))
-    processing.run("native:clip", {'INPUT':'H:/AnkitKailashi/withCode/Analysis/hypsometric/v3Man10ContourPoly20m.shp','OVERLAY':r'H:\AnkitKailashi\withCode\subBasins\%s\%s.shp'%(feature['Name'],feature['Name']),'OUTPUT':'H:/AnkitKailashi/withCode/Analysis/hypsometric/subBasins/%s/v3%shypsometric.shp'%(feature["Name"],feature["Name"])})
-    print("%s is created"%(feature["Name"]))
-    
+    try:
+        
+        os.makedirs(r'F:\pythonProject\QGis\basins\carto10\subBasins\%s\DEM'%(feature['Name'])) #address of our file
     
     
-    #processing.run("native:clip", {'INPUT':'H:/AnkitKailashi/withCode/Analysis/hypsometric/v3Man10ContourPoly20m.shp','OVERLAY':QgsProcessingFeatureSourceDefinition('H:/AnkitKailashi/dem/carto10m/hydrology/bqasin/subBasins/subBasins.shp', selectedFeaturesOnly=True, featureLimit=-1, geometryCheck=QgsFeatureRequest.GeometryAbortOnInvalid),'OUTPUT':'H:/AnkitKailashi/withCode/Analysis/hypsometric/subBasins/Basin1/v3Basin1hypsometric.shp'})
-    # v3Man10SubBasin.removeSelection()
-    #break
-    #print('H:/AnkitKailashi/withCode/Analysis/hypsometric/subBasins/%s/v3%shypsometric.shp'%(feature["Name"],feature["Name"]))
-    #
-iface.messageBar().clearWidgets()
+    
+        write = QgsVectorFileWriter.writeAsVectorFormat(v3Man10SubBasin,r'F:\pythonProject\QGis\basins\carto10\subBasins\%s\carto10%s.shp'%(feature['Name'],feature['Name']), 'utf-8', driverName='ESRI Shapefile',onlySelected=True)
+    
+        
+        #clip slope and drainage of individual sub-basin
+        processing.run("gdal:cliprasterbymasklayer", {'INPUT':r'F:\pythonProject\QGis\basins\carto10\carto10ManSlopDgr.tif','MASK':r'F:\pythonProject\QGis\basins\carto10\subBasins\%s\carto10%s.shp'%(feature['Name'],feature['Name']),'SOURCE_CRS':None,'TARGET_CRS':QgsCoordinateReferenceSystem('EPSG:32643'),'NODATA':None,'ALPHA_BAND':False,'CROP_TO_CUTLINE':True,'KEEP_RESOLUTION':False,'SET_RESOLUTION':False,'X_RESOLUTION':None,'Y_RESOLUTION':None,'MULTITHREADING':False,'OPTIONS':'','DATA_TYPE':0,'EXTRA':'','OUTPUT':r'F:\pythonProject\QGis\basins\carto10\subBasins\%s\DEM\carto10%sSlopeDgr.tif'%(feature['Name'],feature['Name'])})
+        processing.run("gdal:clipvectorbypolygon", {'INPUT':'E:/phd/satelite Images/cartoset DEM/v3/mosaic/arcgis work/stream/v3mandrainage.shp','MASK':r'F:\pythonProject\QGis\basins\carto10\subBasins\%s\carto10%s.shp'%(feature['Name'],feature['Name']),'OPTIONS':'','OUTPUT':r'F:\pythonProject\QGis\basins\carto10\subBasins\%s\DEM\%sDrainage.shp'%(feature['Name'],feature['Name'])})
+        
+    #check any exceptional error
+    except Exception:
+        feat.append(feature['Name'])
 
-iface.showAttributeTable(v3Man10SubBasin)
+
+print(feat)
