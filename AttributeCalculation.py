de0d0433d32f8b5bfd451c75fe8fd58aa9a472a6
Raathore4 <73769538+Raathore4@users.noreply.github.com>
2023-05-15 14:38:20 +0530

    Update and rename fieldCalculator.py to AttributeCalculation.py

--- AttributeCalculation.py
+++ AttributeCalculation.py
@@ -1,7 +1,8 @@
+#call sub-basin file
 subPath = r'F:\pythonProject\QGis\basins\carto10\subBasins\carto10SubBasin.shp'
 sub = QgsVectorLayer(subPath,'Stream Length Gradient','ogr')
 
-#l = ['Basin29','Basin30','Basin27','Basin25','Basin22',]
+
 
 def FieldCalcu():
 
@@ -12,8 +13,10 @@ def FieldCalcu():
         v3ManSlope = QgsRasterLayer(pathR,'v3ManUTM','GDAL')
         pathRv = r'F:\pythonProject\QGis\basins\carto10\subBasins\%s\DEM\carto10%s.tif'%(f['Name'],f['Name'])
         v3ManUtm = QgsRasterLayer(pathRv,'v3ManUTM','GDAL')
-                
-        
+        print("\n%s\n"%(f['Name']))   
+        statsDEM = v3ManUtm.dataProvider().bandStatistics(1)
+        statsSlope = v3ManSlope.dataProvider().bandStatistics(1)
+        f['Relief'] = statsDEM.maximumValue-statsDEM.minimumValue
         featCount = drainage.featureCount()
         total_sum = drainage.aggregate(QgsAggregateCalculator.Sum, "length")
         lengthSum = round(total_sum[0]/1000,2)
@@ -23,49 +26,37 @@ def FieldCalcu():
         loF = 1/(2*f['DraDensity'])
         ReR = f['Relief']/(f['Periferi'])
         Rugd = f['DraDensity']*f['Relief']/1000
-        print("\n%s\n"%(f['Name']))
+        
         RR = f['Relief']/(f['BasinLengt'])
         print("\n%s\tRelative %f \tRR %.2f"%(f['Name'],ReR,RR))
         count = 0
-        #f['ReliefRati'] = round(RR,3)
+        
         for x in drainage.getFeatures():
             if x['order_']==1:
                 count+=1
             else:
                 pass
-        text = count/f['Periferi']
-        #print('\nTexture = ',text)
+        text = count/f['Periferi']       
+        cir = (4*(22/7)*f['Area'])/(f['Periferi']**2)        
+        f['MinEle'] = statsDEM.minimumValue
+        f['MaxEle'] = statsDEM.maximumValue
+        f['MeanEle'] = statsDEM.mean
+        f['TextureRat'] = text
+        f['MxSlope'] = statsSlope.maximumValue
+        f['MeanSlope'] = statsSlope.mean
+        f['ConChaMant'] = coM
+        f['LenOverLan'] = loF
+        f['RelatiReli'] = RR
+        f['Ruggedness'] = Rugd
+        f['Circularit'] = round(cir,2)
+        f['DraDensity'] = dens
+        f['DraFrequen'] = freq
+        f['FormFact'] = f['Form Facto']
+        f['ShapeFact'] = f['Shape Fact']
+        f['ReliefRati'] = round(RR,3)
         
-        #slope study
-        stats = v3ManUtm.dataProvider().bandStatistics(1)
-        #f['FormFact'] = f['Form Facto']
-        #f['ShapeFact'] = f['Shape Fact']
-        #print("f['FormFact']",f['FormFact'])
-        #print("f['ShapeFact']",f['ShapeFact'])
         
-        
-        cir = (4*(22/7)*f['Area'])/(f['Periferi']**2)
-        #print("old cir = %f and New cir %f"%(f['Circularit'],cir))
-        #f['Circularit'] = round(cir,2)
-        '''
-        #print("%s"%(f['Name']))
-        #print("Min value: {}".format(stats.minimumValue))
-        #print("Max value: {}".format(stats.maximumValue))
-        #print("Mean value: {}".format(stats.mean))
-        #f['TextureRat'] = text
-        #f['MeanEle'] = stats.mean
-        #f['MxSlope'] = stats.maximumValue
-        #f['MeanSlope'] = stats.mean
-        #f['ConChaMant'] = coM
-        #f['LenOverLan'] = loF
-        #f['RelatiReli'] = RR
-        #f['Ruggedness'] = Rugd
-        #print("Constant of Channel Maintance",coM,"\nLength of OverlandFlow",loF,"\nRelief Ratio",RR,"\nRuggedness",Rugd)
-        #f['DraDensity'] = dens
-        #f['DraFrequen'] = freq
-        #print("\n\n\n%s \nfeatures = %d \nArea = %.2f \nlength = %.f \nFreq = %.2f and \ndensity = %.2f"%(f['Name'],featCount,f['Area'],lengthSum,freq,dens))
-        '''
-        #sub.updateFeature(f)
+        sub.updateFeature(f)
 
     
 #for check editing
